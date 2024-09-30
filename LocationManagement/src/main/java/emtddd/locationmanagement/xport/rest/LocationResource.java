238f15c23ea71b478d3b340a7260c0b76324e77d
Naskovski <filip.naskovski.hk@gmail.com>
2024-10-01 00:49:26 +0200

    add location by id

--- LocationManagement/src/main/java/emtddd/locationmanagement/xport/rest/LocationResource.java
+++ LocationManagement/src/main/java/emtddd/locationmanagement/xport/rest/LocationResource.java
@@ -1,9 +1,14 @@
 package emtddd.locationmanagement.xport.rest;
 
 import emtddd.locationmanagement.domain.models.Location;
+import emtddd.locationmanagement.domain.models.LocationID;
 import emtddd.locationmanagement.service.LocationService;
+import emtddd.sharedkernel.domain.exceptions.InvalidIdException;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +24,18 @@ public class LocationResource {
     public List<Location> getAll(){
         return locationService.findAll();
     }
+
+    @GetMapping("/id/{locationId}")
+    public ResponseEntity<Location> findById(@PathVariable String locationId) {
+        try {
+            LocationID locationID = new LocationID(locationId);
+            Location location = locationService.findById(locationID).orElseThrow(InvalidIdException::new);
+            return ResponseEntity.ok(location);
+        } catch (InvalidIdException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
 }
